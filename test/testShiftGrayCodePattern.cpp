6499b8a665f7ba88e624da39e1e59bc0f925f318
Liu Yunhuang <1369215984@qq.com>
2024-04-11 18:12:10 +0800

    fix(windows.yml,...): fix workflows and shift gray code method. 1. fix Github Action workflows. 2. finish shift Gray code method. 3. precise test is perferm well.

--- test/testShiftGrayCodePattern.cpp
+++ test/testShiftGrayCodePattern.cpp
@@ -26,4 +26,29 @@ TEST_F(ShiftGrayCodePatternSuit, testGenerate) {
 
     vector<Mat> imgs;
     pattern->generate(imgs);
+
+    ASSERT_EQ(imgs[6].ptr<uchar>(400)[215], 255);
+}
+
+TEST_F(ShiftGrayCodePatternSuit, testUnwrap) {
+    auto params = SinusShiftGrayCodePattern::Params();
+    params.shiftTime = 4;
+    params.height = 1080;
+    params.width = 1920;
+    params.horizontal = false;
+    params.nbrOfPeriods = 32;
+
+    auto pattern = SinusShiftGrayCodePattern::create(params);
+
+    vector<Mat> imgs;
+    pattern->generate(imgs);
+
+    Mat wrapPhaseMap, floorMap, confidenceMap, unwrapPhaseMap;
+    pattern->computePhaseMap(imgs, wrapPhaseMap);
+    pattern->computeConfidenceMap(imgs, confidenceMap);
+    pattern->computeFloorMap(imgs, confidenceMap, wrapPhaseMap, floorMap);
+    pattern->unwrapPhaseMap(wrapPhaseMap, floorMap, unwrapPhaseMap);
+
+    ASSERT_EQ(floorMap.ptr<uint16_t>(444)[780], 12);
+    ASSERT_EQ(floorMap.ptr<uint16_t>(444)[781], 13);
 }
